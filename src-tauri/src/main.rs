53467a8a3b302f95f3e420e1c4d555398d1e182a
Pronoun <pronoun.nationstates@gmail.com>
2022-09-10 16:31:33 -0700

    feat: add encryption backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,64 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
+
+use chacha20poly1305::{
+	aead::{ Aead, AeadCore, KeyInit, OsRng, generic_array::GenericArray },
+	ChaCha20Poly1305,
+};
+use argon2::{ password_hash::{ SaltString }, Argon2 };
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+	#[error(transparent)] Argon2(#[from] argon2::Error),
+	#[error(transparent)] Argon2hash(#[from] argon2::password_hash::Error),
+	#[error(transparent)] FromUtf8Error(#[from] std::string::FromUtf8Error),
+	#[error(transparent)] Chacha20poly1305(#[from] chacha20poly1305::Error),
+	#[error(transparent)] InvalidLength(#[from] crypto_common::InvalidLength),
+}
+
+impl serde::Serialize for Error {
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: serde::ser::Serializer {
+		serializer.serialize_str(self.to_string().as_ref())
+	}
+}
+
+#[tauri::command]
+fn encrypt(data: String, password: &[u8], salt: &[u8]) -> Result<(Vec<u8>, Vec<u8>), Error> {
+	let key = get_key(password, salt)?;
+	let cipher = ChaCha20Poly1305::new_from_slice(&key)?;
+	let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng);
+	let ciphertext = cipher.encrypt(&nonce, data.as_bytes())?;
+
+	Ok((ciphertext, nonce.to_vec()))
+}
+
+#[tauri::command]
+fn decrypt(data: Vec<u8>, password: &[u8], salt: &[u8], nonce: Vec<u8>) -> Result<String, Error> {
+	let key = get_key(password, salt)?;
+	let cipher = ChaCha20Poly1305::new_from_slice(&key)?;
+	let nonce = GenericArray::from_slice(nonce.as_slice());
+	let plaintext = cipher.decrypt(&nonce, data.as_slice())?;
+
+	Ok(String::from_utf8(plaintext)?)
+}
+
+#[tauri::command]
+fn generate_salt() -> Result<String, Error> {
+	Ok(SaltString::generate(&mut OsRng).to_string())
+}
+
+fn get_key(password: &[u8], salt: &[u8]) -> Result<[u8; 32], Error> {
+	let argon2 = Argon2::default();
+	let mut hash = [0u8; 32];
+
+	argon2.hash_password_into(password, salt, &mut hash)?;
+
+	Ok(hash)
+}
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+	tauri::Builder
+		::default()
+		.invoke_handler(tauri::generate_handler![encrypt, decrypt, generate_salt])
+		.run(tauri::generate_context!())
+		.expect("error while running tauri application");
 }
